b126642ba404a6bcc81dba7d708af6ad284c409f
Ahmad Ibrahim <iahmd01@gmail.com>
2024-03-17 22:22:04 +0300

    [Autoupdate] DbmsConnection.session.sql update.sh

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ echo "Pulled"
 files=$(find . -name '*.sql' -or -name '*.ra' -or -name '*.datalog' -or -name '*.drawio' -or -name '*.md' -or -name '*.png')
 
 git add $files
-git commit -a -m "[Autoupdate] $files"
+git commit -a -m "[Autoupdate] $(git diff --name-only HEAD)"
 echo "Commited changes"
 
 git push
